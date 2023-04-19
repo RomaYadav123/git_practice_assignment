2dba7204d7d28a41a9ea7ae9b0df7477e7eed40a
sarvesh-masai <sarveshmsai@gmail.com>
2023-04-19 19:27:50 +0530

    code_fix

--- PrimeNo.js
+++ PrimeNo.js
@@ -1,12 +1,12 @@
 let a= 15;
 factor= 0;
-for(let i=0; i>=a; i++){
-if(a%i==0){
-factor++;
-}
+for(let i=1; i<=a; i++){
+ if(a%i==0){
+  factor++;
+ }
 }
 if(factor==2){
-console.log("Prime");
-} else{
-console.log("Not");
+ console.log("Prime");
+}else{
+ console.log("Not");
 }
